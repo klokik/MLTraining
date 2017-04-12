Somewhat fix backpropagation, use l_2 loss function, change plotting colors and range

nn/multilayer_perceptron.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 traindata = [ ([2,1], 0),
               ([1,1], 0),
-              #([0,0], 0),
+              ([0,0], 0),
               ([0.8,1.4], 0),
               ([2.5,2], 0),
               ([0,1], 1),
@@ -13,9 +13,8 @@ traindata = [ ([2,1], 0),
               ([0.6,2.2], 1),
               ([-1,-1], 1) ];
 
-ls = [2, 10, 2]
+ls = [2, 8, 8, 2]
 
-out = [np.zeros(ls[i]) for i in range(0, len(ls))]
 
 def newWeights():
   return [np.random.rand(ls[i], ls[i-1]+1)*2-1 for i in range(1, len(ls))]
@@ -36,29 +35,44 @@ def dSigmoid(x):
   return (1-sigmoid(x))*sigmoid(x)
 
 def forward(x, W):
+  out = [np.zeros(ls[i]) for i in range(0, len(ls))]
   out[0] = x
   for i in range(1, len(out)):
     inp = np.concatenate(([1],out[i-1]))
     out[i] = np.matmul(W[i-1], inp)
     for j,v in enumerate(out[i]):
       out[i][j] = ReLU(out[i][j])
-  return out[-1]
+  return out
+
+def Err(x, y):
+  return np.sum(np.power(np.subtract(x, y), 2))/2
+
+def dErr(x, y, n):
+  return x[n] - y[n]
 
 def backprop(nn_out, cl_expected, W, rate):
   gr_out = [np.zeros(ls[i]+1) for i in range(0, len(ls))]
 
   expected = np.zeros(ls[-1])
   expected[cl_expected] = 1
-  gr_out[-1][1:] = np.subtract(nn_out, expected)
+  #gr_out[-1][1:] = np.subtract(nn_out, expected)
+  #gr_out[-1][1:] = np.ones(ls[-1])
+  for i in range(0, ls[-1]):
+    gr_out[-1][i+1] = dErr(nn_out[-1], expected, i)#dLoss_i(nn_out[-1], i, cl_expected)
 
   for i in range(len(ls)-1, 0, -1):
-    x = np.concatenate(([1],out[i-1]))
+    x = np.concatenate(([1],nn_out[i-1]))
     for j in range(0, ls[i]):
+      dL_x_ds = gr_out[i][j+1]*dReLU(np.dot(x, W[i-1][j]))
+      #print(dL_x_ds)
       for k in range(0, ls[i-1]+1):
-        dxk = gr_out[i][j]*dReLU(np.dot(x, W[i-1][j]))*W[i-1][j][k]
+        dxk = dL_x_ds*W[i-1][j][k]
         gr_out[i-1][k] += dxk
-        dwjk = gr_out[i][j]*dReLU(np.dot(x, W[i-1][j]))*x[k]
+        dwjk = dL_x_ds*x[k]
         W[i-1][j][k] -= dwjk*rate
+    #print(gr_out[i])
+  #print(gr_out)
+  #exit(0)
 
 def loss_i(y, n):
   # softmax
@@ -68,10 +82,35 @@ def loss_i(y, n):
 def loss(W):
   acc = 0
   for item in traindata:
-    y = forward(item[0], W)
+    y = forward(item[0], W)[-1]
     acc += loss_i(y, item[1])
   # TODO: regularization
+  return acc #/len(traindata)
+
+def lossE(W):
+  acc = 0
+  for item in traindata:
+    y = forward(item[0], W)[-1]
+    expected = np.zeros(ls[-1])
+    expected[item[1]] = 1
+    acc += Err(y, expected)
   return acc
+
+def dLoss_i(y, k, n):
+  h = 0.001
+  l_l = loss_i(y, n)
+  y[k] += h
+  l_r = loss_i(y, n)
+  y[k] -= h
+
+  return (l_r-l_l)/h
+  #bias = np.max(y)
+  #S = np.sum(np.exp(y-bias))
+
+  #if k != n:
+  #  return -np.exp(y[k])/S
+  #else:
+  #  return 1-np.exp(y[k])/S
 
 def getGradAtPoint(f, x):
   grad = [np.zeros(li.shape) for li in x]
@@ -94,7 +133,7 @@ def getGradAtPoint(f, x):
   return grad
 
 def learn_random(n):
-  bestW = np.multiply(newWeights(), 0.01)
+  bestW = np.multiply(newWeights(), 0.1)
   bestLoss = 100000
   for k in range(n):
     newW = newWeights()
@@ -109,7 +148,7 @@ def learn_random(n):
   return bestW
 
 def learn_random_gradient(n):
-  bestW = learn_random(n/10)
+  bestW = learn_random(int(n/10))
   bestLoss = 100000
   for k in range(n):
     newW = np.add(bestW, np.multiply(newWeights(), 0.01))
@@ -124,9 +163,9 @@ def learn_random_gradient(n):
   return bestW
 
 def learn_num_gradient(n):
-  W = learn_random(n/3)
+  W = learn_random(int(n/3))
   bestLoss = 100000
-  learn_rate = 0.01
+  learn_rate = 0.005
   for k in range(n):
     grad = getGradAtPoint(loss, W);
     W = np.subtract(W, np.multiply(grad, learn_rate))
@@ -139,14 +178,14 @@ def learn_num_gradient(n):
   return W
 
 def learn_backprop(n):
-  W = learn_random(n/10)
+  W = learn_random(int(n/3))
   for k in range(n):
     for item in traindata:
-      y = forward(item[0], W)
-      backprop(nn_out=y, cl_expected=item[1], W=W, rate=0.001)
+      nn_out = forward(item[0], W)
+      backprop(nn_out=nn_out, cl_expected=item[1], W=W, rate=0.01)
 
     c_loss = loss(W)
-    if k % 1 == 0:
+    if k % 10 == 0:
       print("sample {0}, loss: {1}".format(k, c_loss))
       bestLoss = c_loss
 
@@ -154,17 +193,17 @@ def learn_backprop(n):
   return W
 
 def main():
-  # W = learn_random_gradient(50000)
-  W = learn_backprop(10)
+  #W = learn_random_gradient(1000)
+  W = learn_backprop(1000)
 
-  xs = np.linspace(-3, 3, num=100)
-  ys = np.linspace(-3, 3, num=100)
+  xs = np.linspace(-2, 4, num=100)
+  ys = np.linspace(-2, 4, num=100)
   xxs, yys = np.meshgrid(xs, ys)
   zs = np.zeros((xs.size, ys.size))
 
   for i,x in enumerate(xs):
     for j,y in enumerate(ys):
-      z = forward([x,y], W)
+      z = forward([x,y], W)[-1]
       if z[0] > z[1]:
         zs[j, i] = 0
       else:
@@ -176,7 +215,7 @@ def main():
     ptx = [item[0][0]]
     pty = [item[0][1]]
     if item[1] == 0:
-      plt.plot(ptx, pty, "bo")
+      plt.plot(ptx, pty, "ro")
     else:
       plt.plot(ptx, pty, "gs")
   #plt.show(block=False)